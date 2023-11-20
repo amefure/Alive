3e03d3781c12d21b130a9b9347766f7a1562be2d
amefure <shachi10969@gmail.com>
2023-11-20 19:05:36 +0900

    Initial Commit

---
+++ Alive/AliveApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliveApp.swift
+//  Alive
+//
+//  Created by t&a on 2023/11/20.
+//
+
+import SwiftUI
+
+@main
+struct AliveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

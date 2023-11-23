9ab0ee5ddfd6c1b83082ca12a4cd9eb27ead87ed
amefure <shachi10969@gmail.com>
2023-11-23 20:43:21 +0900

    チケットUIの実装

--- Alive/View/LiveScheduleListView.swift
+++ Alive/View/LiveScheduleListView.swift
@@ -28,6 +28,7 @@ struct LiveScheduleListView: View {
             VStack {
                 HeaderView()
                 
+                NextLiveView(live: repository.lives.sorted(by: { $0.date < $1.date }).last)
                 LiveHistoryBlockView(array: repository.generateAvailability)
                 
                 HStack {
@@ -75,6 +76,115 @@ struct LiveScheduleListView: View {
     }
 }
 
+struct NextLiveView:View {
+    public var live: Live?
+    private let imageFileManager = ImageFileManager()
+    private let dateFormatManager = DateFormatManager()
+    var body: some View {
+        if let live = live {
+
+            VStack(spacing:0) {
+                    
+                    HStack {
+                        
+                        if let image = imageFileManager.loadImage(name: live.imagePath) {
+                            
+                            Image(uiImage: image)
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                                .background(.foundation)
+                                .clipShape(RoundedRectangle(cornerRadius: 50))
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 50)
+                                        .stroke(.white, lineWidth: 3)
+                                    
+                                ).padding(.leading, 20)
+                            
+                        } else {
+                            Asset.Images.appLogoElectric.swiftUIImage
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                                .background(.foundation)
+                                .clipShape(RoundedRectangle(cornerRadius: 50))
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 50)
+                                        .stroke(.white, lineWidth: 3)
+                                    
+                                ).padding(.leading, 20)
+                                
+                        }
+                    
+                        
+                        Text(live.name)
+                            .frame(height: 100)
+                            .font(.system(size: 20))
+                            .padding(.leading, 20)
+                        
+                        Spacer()
+                        
+                    }
+                    ZigzagBottomLine()
+                        .fill(Color.white)
+                        .frame(width: DeviceSizeManager.deviceWidth - 55, height: 1)
+                    VStack {
+                        HStack {
+                            
+                            VStack {
+                                HStack {
+                                    Image(systemName: "music.mic")
+                                        .padding(.leading, 20)
+                                    Text(live.artist)
+                                    Spacer()
+                                }
+                                
+                                HStack {
+                                    Image(systemName: "mappin.and.ellipse")
+                                        .padding(.leading, 20)
+                                    Text(live.venue)
+                                    Spacer()
+                                }
+                            }
+                            
+                            
+                            Text(dateFormatManager.getShortString(date: live.date))
+                                .font(.largeTitle)
+                                .padding(.trailing, 20)
+                        }
+                    }.frame(height: 100)
+                    .background(Color.white)
+                        .foregroundStyle(.foundation)
+                        
+                    
+                }.background(.themaYellow)
+                .frame(width: DeviceSizeManager.deviceWidth - 40)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .fontWeight(.bold)
+
+        } else {
+            
+        }
+    }
+}
+
+
+struct ZigzagBottomLine: Shape {
+    var zigzagSpacing: CGFloat = 25.0
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.height))
+
+        for x in stride(from: 0, to: rect.width, by: zigzagSpacing) {
+            path.addLine(to: CGPoint(x: x + zigzagSpacing / 2, y: rect.height - zigzagSpacing * 0.3))
+            path.addLine(to: CGPoint(x: x + zigzagSpacing, y: rect.height))
+        }
+
+        return path
+    }
+}
+
+
 struct LiveHistoryBlockView: View {
     
     public var array: [Bool]
@@ -115,5 +225,6 @@ struct LiveHistoryBlockView: View {
 }
 
 #Preview {
-    LiveScheduleListView()
+//    LiveScheduleListView()
+    NextLiveView(live: Live.demoLive)
 }
